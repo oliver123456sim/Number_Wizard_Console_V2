b029f554c99ba0f9191569fdcd4873fe388e9300
Oliver <oliverpsimonsen@gmail.com>
2019-01-31 09:55:50 +0100

    ÆNDRING KLIK PÅ KNAP
    Jeg har lavet lidt om i mit number_wizard skript

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    int forsøg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
